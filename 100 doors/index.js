23d9cfee85b80eb9a9cc3f7cb64d76abc78d38aa
TheSilentOwl <115552526+TheSilentOwl@users.noreply.github.com>
2024-09-13 09:59:28 +0300

    initial project setup, and build thought process for 100 doors project

---
+++ 100 doors/index.js
@@ -0,0 +1,10 @@
+function getFinalOpenedDoors(numDoors) {
+    // BRUTE FORCE SOLUTION
+    // =====================
+    
+      //create array of 100 closed doors
+    
+      //create a loop that will start at each of the numbers from 1 to 100: each time you will be positioned at that number and if less than 100 incremented by an amount equivalent to that number (for example start at 2, and increment by 2 -> 4, 6, 8, .., 100) and through each increment change the state of the current corresponding door
+    
+      //after the you exit the loop return the array of the states of doors!
+    }
